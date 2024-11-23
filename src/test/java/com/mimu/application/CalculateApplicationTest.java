b70f99c542633c10230c4602d492a755f5908595
mimu <primaryscholar@126.com>
2024-11-23 14:31:44 +0800

    feat:
    完善项目

---
+++ src/test/java/com/mimu/application/CalculateApplicationTest.java
@@ -0,0 +1,10 @@
+package com.mimu.application;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+public class CalculateApplicationTest {
+
+}
